b19bdb53544753ce6da7fc5ef5aad710033449d1
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-22 17:21:31 +0300

    gradient

--- HowToUseGradientInSwiftUI/ContentView.swift
+++ HowToUseGradientInSwiftUI/ContentView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        ZStack {
+            
+            LinearGradient(gradient: .init(colors: [.purple, .pink, .purple]), startPoint: .bottomLeading, endPoint: .topTrailing)
+                .edgesIgnoringSafeArea(.all)
+        
+            GeometryReader { reader in
+                    
+                Button(action: {
+                    
+                }) {
+                    Text("Register")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding()
+                }.frame(width: reader.size.width / 2)
+                    .background(LinearGradient(gradient: .init(colors: [.orange, .red]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(25)
+                .shadow(radius: 25)
+                
+                
+                
+            }
+        }
     }
 }
 
@@ -19,3 +42,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
